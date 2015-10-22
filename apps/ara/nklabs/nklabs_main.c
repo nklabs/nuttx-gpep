3bee455c4b0fbecdc3b401ebd186d477daf4e725
Myron King <mdk@nklabs.com>
2015-10-22 15:37:45 -0400

    preping gpio 24 for pld testing

--- apps/ara/nklabs/nklabs_main.c
+++ apps/ara/nklabs/nklabs_main.c
@@ -34,31 +34,44 @@
 #include <string.h>
 #include <unistd.h>
 
+# define getreg32(a)          (*(volatile uint32_t *)(a))
+# define putreg32(v,a)        (*(volatile uint32_t *)(a) = (v))
+
+#define GPIO_BASE           0x40003000
+#define GPIO_DATA           (GPIO_BASE)
+#define GPIO_ODATA          (GPIO_BASE + 0x4)
+#define GPIO_ODATASET       (GPIO_BASE + 0x8)
+#define GPIO_ODATACLR       (GPIO_BASE + 0xc)
+#define GPIO_DIR            (GPIO_BASE + 0x10)
+#define GPIO_DIROUT         (GPIO_BASE + 0x14)
+#define GPIO_DIRIN          (GPIO_BASE + 0x18)
 
 int nklabs_main(int argc, char **argv) {
 
-    int read = 0;
-    unsigned int addr = 0;
-    unsigned int val = 0;
-    char *op = argv[1];
+    /* int read = 0; */
+    /* unsigned int addr = 0; */
+    /* unsigned int val = 0; */
+    /* char *op = argv[1]; */
 
-    if (!strcmp(op, "read") || !strcmp(op, "r")) {
-      read = 1;
-    } else if (!strcmp(op, "write") || !strcmp(op, "w")) {
-      val  = strtoul(argv[3], NULL, 16);
-    } else {
-      printf("unrecognized operation\n");
-    }
-    addr = strtoul(argv[2], NULL, 16);
+    /* if (!strcmp(op, "read") || !strcmp(op, "r")) { */
+    /*   read = 1; */
+    /* } else if (!strcmp(op, "write") || !strcmp(op, "w")) { */
+    /*   val  = strtoul(argv[3], NULL, 16); */
+    /* } else { */
+    /*   printf("unrecognized operation\n"); */
+    /* } */
+    /* addr = strtoul(argv[2], NULL, 16); */
 
-    printf("op = %s\naddr = %x\nval=%x\n", read ? "Read" : "Write", addr, val);
-    printf("...\n");
+    /* printf("op = %s\naddr = %x\nval=%x\n", read ? "Read" : "Write", addr, val); */
+    /* printf("...\n"); */
 
-    if(read){
-      printf("%x\n", *((volatile unsigned int*)addr));
-    } else {
-      *((volatile unsigned int*)addr) = val;
-    }
+    /* if(read){ */
+    /*   printf("%x\n", *((volatile unsigned int*)addr)); */
+    /* } else { */
+    /*   *((volatile unsigned int*)addr) = val; */
+    /* } */
 
-    return 0;
+  putreg32(1 << 24, GPIO_ODATASET );
+  putreg32(1 << 24, GPIO_DIROUT );
+  return 0;
 }
